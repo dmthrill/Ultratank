8fc618f1e8f9ebb53aa479e500f4d86488e7a489
Dmitry B <dmitryboremsky@gmail.com>
2024-06-14 13:54:40 +0300

    read 1 rxchannel with interrupt

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,45 @@
 #include <Arduino.h>
+
 const int ch1 = 15;
-const int ch2 = 2; 
-const int ch3 = 4;
-const int ch4 = 16;
-const int ch5 = 17;
-const int ch6 = 5;
-const int ch7 = 18;
-const int ch8 = 19;
-const int ch9 = 21;
-const int ch10 = 22;
+// ... остальные пины
+
+volatile unsigned long risingEdge[10];
+volatile unsigned long fallingEdge[10];
+
+void IRAM_ATTR handleInterrupt0(void* arg) {
+  static uint32_t prevRise = 0;
+  static uint32_t prevFall = 0;
+  uint32_t currTime = micros();
+  
+  if (digitalRead(ch1) == HIGH) {
+    prevRise = currTime;
+  } else {
+    prevFall = currTime;
+    risingEdge[0] = prevRise;
+    fallingEdge[0] = prevFall;
+  }
+}
+
+// Похожие функции handleInterrupt1, handleInterrupt2, ... для остальных каналов
 
 void setup() {
-  pinMode(ch1, INPUT); 
-  pinMode(ch2, INPUT);
-  pinMode(ch3, INPUT);
-  pinMode(ch4, INPUT);
-  pinMode(ch5, INPUT);
-  pinMode(ch6, INPUT);
-  pinMode(ch7, INPUT);
-  pinMode(ch8, INPUT);
-  pinMode(ch9, INPUT);
-  pinMode(ch10, INPUT);
-
   Serial.begin(115200);
+  
+  // Attach interrupts
+  attachInterruptArg(ch1, handleInterrupt0, NULL, CHANGE);
+  // ... остальные пины
+
+  // Enable interrupts
+  interrupts();
 }
 
 void loop() {
-  unsigned long pwmRX1 = pulseIn(ch1, HIGH);
-  unsigned long pwmRX2 = pulseIn(ch2, HIGH);
-  unsigned long pwmRX3 = pulseIn(ch3, HIGH);
-  unsigned long pwmRX4 = pulseIn(ch4, HIGH);
-  unsigned long pwmRX5 = pulseIn(ch5, HIGH);
-  unsigned long pwmRX6 = pulseIn(ch6, HIGH);
-  unsigned long pwmRX7 = pulseIn(ch7, HIGH);
-  unsigned long pwmRX8 = pulseIn(ch8, HIGH);
-  unsigned long pwmRX9 = pulseIn(ch9, HIGH);
-  unsigned long pwmRX10 = pulseIn(ch10, HIGH);
-  
-  // Вывод значения pwmValue в Serial порт
-  Serial.print("ch1: ");
-  Serial.print(pwmRX1);
-  Serial.print(", ch2: ");
-  Serial.print(pwmRX2);
-  Serial.print(", ch3: ");
-  Serial.print(pwmRX3);
-  Serial.print(", ch4: ");
-  Serial.print(pwmRX4);
-  Serial.print(", ch5: ");
-  Serial.print(pwmRX5);
-  Serial.print(", ch6: ");
-  Serial.print(pwmRX6);
-  Serial.print(", ch7: ");
-  Serial.print(pwmRX7);
-  Serial.print(", ch8: ");
-  Serial.print(pwmRX8);
-  Serial.print(", ch9: ");
-  Serial.print(pwmRX9);
-  Serial.print(", ch10: ");
-  Serial.print(pwmRX10);
-  Serial.print("\n");
- 
-  // Здесь можно добавить другую обработку pwmValue
-
-  
+  for (int i = 0; i < 10; i++) {
+    if (fallingEdge[i] > risingEdge[i]) {
+      unsigned long pulseDuration = fallingEdge[i] - risingEdge[i];
+      Serial.printf("ch%d: %lu\n", i + 1, pulseDuration);
+      risingEdge[i] = 0;
+      fallingEdge[i] = 0;
+    }
+  }
 }
